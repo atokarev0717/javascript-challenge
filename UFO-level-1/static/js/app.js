ccc84e7f5ed25333ab1623c01be313d4c63be80f
Andrey Tokarev <atokarev1@gmail.com>
2021-03-01 15:24:48 -0700

    added data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,22 @@
 // from data.js
 var tableData = data;
+
+
+// YOUR CODE HERE!
 console.log(tableData);
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
 
+tableData.forEach(function(ufo) {
+  console.log(ufo);
+  var row = tbody.append("tr");
 
+  Object.entries(ufo).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+    var list = d3.select(".table table-striped");
+    list.append(tableData);
+  });
+});
